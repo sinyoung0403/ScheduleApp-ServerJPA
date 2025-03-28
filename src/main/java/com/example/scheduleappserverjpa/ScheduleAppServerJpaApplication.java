8a42371cfcac8d37e9e1ec8670b1d7b6cb906cd2
sinyoung0403 <tlstjs753@gmail.com>
2025-03-28 17:09:39 +0900

    init: 프로젝트 초기 설정 #00

---
+++ src/main/java/com/example/scheduleappserverjpa/ScheduleAppServerJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.scheduleappserverjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleAppServerJpaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ScheduleAppServerJpaApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/scheduleappserverjpa/ScheduleAppServerJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scheduleappserverjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleAppServerJpaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
