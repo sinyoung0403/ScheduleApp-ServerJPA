72809b50c8bf7c8f64f8bb551235c30c6fae29fa
sinyoung0403 <tlstjs753@gmail.com>
2025-04-01 20:00:50 +0900

    feat: Level08 PageInfo 정보 Map 으로 추가 #12

--- src/main/java/com/example/scheduleappserverjpa/dto/plan/PageResponseDto.java
+++ src/main/java/com/example/scheduleappserverjpa/dto/plan/PageResponseDto.java
@@ -3,19 +3,23 @@ package com.example.scheduleappserverjpa.dto.plan;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Getter
 @AllArgsConstructor
 public class PageResponseDto {
   private List<PlanWithUserAndCommentDto> plans;
-  private int pageNumber;
-  private int pageSize;
-  private int totalPages;
-  private long totalElements;
+  private Map<String, Object> pageInfo;
 
   // from 코드
   public static PageResponseDto from(List<PlanWithUserAndCommentDto> plans, int pageNumber, int pageSize, int totalPages, long totalElements) {
-    return new PageResponseDto(plans, pageNumber+1, pageSize, totalPages, totalElements);
+    Map<String, Object> pageInfo = new LinkedHashMap<>();
+    pageInfo.put("pageNumber", pageNumber + 1);
+    pageInfo.put("pageSize", pageSize);
+    pageInfo.put("totalPages", totalPages);
+    pageInfo.put("totalElements", totalElements);
+    return new PageResponseDto(plans, pageInfo);
   }
 }
